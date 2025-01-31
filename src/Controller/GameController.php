9e9df5e3597a3edf8c4e205a505a83708198aae8
mahedes <mahevadessart@gmail.com>
2025-01-31 21:45:07 +0100

    03_07

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -13,10 +13,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
+use Nelmio\ApiDocBundle\Attribute\Model;
+use Nelmio\ApiDocBundle\Attribute\Security;
+use OpenApi\Attributes as OA;
+
 
 final class GameController extends AbstractController
 {
-    #[Route('/api', name: 'app_game')]
+    /*#[Route('/api', name: 'app_game')]
     public function index(): JsonResponse
     {
         $data = [
@@ -26,11 +30,31 @@ final class GameController extends AbstractController
 
         //return $this->json($data);
         return new JsonResponse($data, Response::HTTP_OK, [], false);
-    }
-
-
+    }*/
 
     #[Route('/api/games', name: 'app_show_games', methods: ['GET'])]
+    #[OA\Response(
+        response: 200,
+        description: 'Retourne la liste des jeux',
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(ref: new Model(type: Game::class, groups: ['full']))
+        )
+    )]
+    #[OA\Parameter(
+        name: 'page',
+        in: 'query',
+        description: 'Numéro de la page désiré',
+        schema: new OA\Schema(type: 'string')
+    )]
+    #[OA\Parameter(
+        name: 'limit',
+        in: 'query',
+        description: 'Nombre de jeux désirés',
+        schema: new OA\Schema(type: 'string')
+    )]
+    #[OA\Tag(name: 'Read')]
+    #[Security(name: 'bearerAuth')]
     public function showGames(Request $request, SerializerInterface $serializer, GameRepository $gameRepository): JsonResponse
     {
         $numPage = $request->get('page', 1);
@@ -48,6 +72,16 @@ final class GameController extends AbstractController
 
 
     #[Route('/api/games/{id}', name: 'app_show_game_details', methods: ['GET'])]
+    #[OA\Tag(name: 'Read')]
+    #[Security(name: 'bearerAuth')]
+    #[OA\Response(
+        response: 200,
+        description: 'Retourne les détails d\'un jeu',
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(ref: new Model(type: Game::class, groups: ['full']))
+        )
+    )]
     public function showDetailsGame(int $id, SerializerInterface $serializer, GameRepository $gameRepository): JsonResponse
     {
         $game = $gameRepository->find($id);
@@ -63,6 +97,17 @@ final class GameController extends AbstractController
 
 
     #[Route('/api/games', name: "app_add_game", methods: ['POST'])]
+    #[OA\Tag(name: 'Create')]
+    #[Security(name: 'bearerAuth')]
+    #[OA\Response(
+        response: 201,
+        description: 'Jeu créé avec succès',
+        content: new OA\JsonContent(ref: new Model(type: Game::class, groups: ['full']))
+    )]
+    #[OA\RequestBody(
+        description: 'Données du jeu à ajouter',
+        content: new OA\JsonContent(ref: new Model(type: Game::class, groups: ['full']))
+    )]
     public function addNewGame(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, DeveloperRepository $developerRepository): JsonResponse
     {
 
@@ -85,6 +130,17 @@ final class GameController extends AbstractController
 
 
     #[Route('/api/games/{id}', name: "app_update_game", methods: ['PUT'])]
+    #[OA\Tag(name: 'Update')]
+    #[Security(name: 'bearerAuth')]
+    #[OA\Response(
+        response: 200,
+        description: 'Met à jour les informations d\'un jeu',
+        content: new OA\JsonContent(ref: new Model(type: Game::class, groups: ['full']))
+    )]
+    #[OA\RequestBody(
+        description: 'Données du jeu à mettre à jour',
+        content: new OA\JsonContent(ref: new Model(type: Game::class, groups: ['full']))
+    )]
     public function updateGames(Game $currentGame, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, DeveloperRepository $developerRepository): JsonResponse
     {
         $updatedGame = $serializer->deserialize(
@@ -109,6 +165,8 @@ final class GameController extends AbstractController
 
 
     #[Route('/api/games/{id}', name: 'app_delete_game', methods: ['DELETE'])]
+    #[OA\Tag(name: 'Delete')]
+    #[Security(name: 'bearerAuth')]
     public function deleteGame(Game $game, EntityManagerInterface $entityManager): JsonResponse
     {
         $entityManager->remove($game);
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,11 +11,23 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-
+use Nelmio\ApiDocBundle\Attribute\Model;
+use Nelmio\ApiDocBundle\Attribute\Security;
+use OpenApi\Attributes as OA;
 
 final class RegistrationController extends AbstractController
 {
     #[Route('/api/register', name: 'app_register', methods: ['POST'])]
+    #[OA\Tag(name: 'Registration')]
+    #[OA\Response(
+        response: 201,
+        description: 'Utilisateur créé avec succès',
+        content: new OA\JsonContent(ref: new Model(type: User::class, groups: ['full']))
+    )]
+    #[OA\RequestBody(
+        description: 'Données de l\'utilisateur à ajouter',
+        content: new OA\JsonContent(ref: new Model(type: User::class, groups: ['full']))
+    )]
     public function register(ValidatorInterface $validator, Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
     {
         $user = $serializer->deserialize($request->getContent(), User::class, 'json');
